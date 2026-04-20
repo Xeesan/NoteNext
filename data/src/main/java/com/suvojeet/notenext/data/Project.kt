127e7c0eba13233a255f2bf11167bc3d28bb7c92
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-04-20 23:13:25 +0530

    perf: add @Immutable to Project and Label data classes

--- data/src/main/java/com/suvojeet/notenext/data/Project.kt
+++ data/src/main/java/com/suvojeet/notenext/data/Project.kt
@@ -1,9 +1,11 @@
 package com.suvojeet.notenext.data
 
+import androidx.compose.runtime.Immutable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
+@Immutable
 @Serializable
 @Entity(tableName = "projects")
 data class Project(
